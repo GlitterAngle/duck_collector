b08b944e8473526a9d6e600c7abec9b0b398290b
cd <lesleyrotonto@gmail.com>
2024-02-05 16:35:25 -0500

    models and serializers

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 # import Home view from the views file
-from .views import Home
+from .views import Home, DuckList, DuckDetail
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  # this allows you to get and post
+  path('ducks/', DuckList.as_view(), name='cat-list'),
+  # this allows you to create delete and put
+  path('cats/<int:id>', DuckDetail.as_view(), name='duck-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Duck
+from .serializers import DuckSerializer
 
 # Define the home view
 class Home(APIView):
   def get(self, request):
     content = {'message': 'Welcome to the duck-collector api home route!'}
     return Response(content)
+
+class DuckList(generics.ListCreateAPIView):
+  queryset = Duck.objects.all()
+  serializer_class = DuckSerializer
+
+class DuckDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Duck.objects.all()
+  serializer_class = DuckSerializer
+  lookup_field = 'id'
+
